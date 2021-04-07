67ca6bcd512f0107cfda7c3c55b02ae33ab2c3c6
Deepak <Deeepak9018@gmail.com>
2021-04-08 03:40:17 +0530

    Shop Registration pg 1 Data is stored in session

--- Coding/php/Add_More_Shop_Images.php
+++ Coding/php/Add_More_Shop_Images.php
@@ -2,11 +2,9 @@
 
 
 session_start();
-
-if (isset($_POST['submit']))
+if (isset($_POST['next']))
 {
-    //creating session for perserving form data.
-
+    echo $_SESSION['info']['txtShopName'];
     foreach($_POST as $key => $value )
     {
         $_SESSION['info'][$key] = $value;
@@ -14,12 +12,14 @@ if (isset($_POST['submit']))
 
     $key = array_keys($_SESSION['info']);
 
-    if(in_array('submit', $keys))
+    if(in_array('next', $keys))
     {
-        unset($_SESSION['info']['submit']);
+        unset($_SESSION['info']['next']);
     }
-
-    header("Location:../index.html");
+    echo "<pre>";
+    print_r ($_SESSION['info']);
+    echo "</pre>";
+    // header("Location:../index.html");
 
 }
 
