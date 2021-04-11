9e9a5b8d57afe74754d48682e1e22005b37e7ea5
Deepak <Deeepak9018@gmail.com>
2021-04-11 16:47:16 +0530

    DB is accepting Multi shop IMGs

--- Coding/php/Add_More_Shop_Images.php
+++ Coding/php/Add_More_Shop_Images.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+/*
 include 'database/connection.php';
 
 // echo"Hello!! \n Welcome to php.";
@@ -51,7 +52,69 @@ if (isset($_POST['submit'])) {
     }
 }
 
+?>
+*/
+ 
+
+
+include 'database/connection.php';
+
+// echo"Hello!! \n Welcome to php.";
+if(isset($_POST['submit'])) {
+
+    //array for Valid Shop Image extension
+    $extension = array('jpeg','jpg','png','gif','tiff','psd','eps','ai','indd','raw');
+
+    foreach ($_FILES['txtshopImages']['tmp_name'] as $key => $value) {
+         $fileName = $_FILES['txtshopImages']['name'][$key];
+        $fileName_tmp = $_FILES['txtshopImages']['tmp_name'][$key];
+        echo '<br>';
+        //printing extension of files
+
+         $shopImgExt = pathinfo($fileName, PATHINFO_EXTENSION);
+
+      //  $finalImg='';
+
+        if(in_array($shopImgExt,$extension))
+        {
+            if(!file_exists('../images/registerShopImages/'.$fileName))
+            {
+                move_uploaded_file($fileName_tmp,'../images/registerShopImages/'.$fileName);
+                $finalImg ="../images/registerShopImages/" . $fileName;
+            }
+            else
+            //if file exists then upload it with by appending current system time.
+            {
+               $fileName = str_replace('.','-',basename($fileName,$shopImgExt));
+               $newFileName = $fileName.time().".".$shopImgExt;
+
+               move_uploaded_file($fileName_tmp,'../images/registerShopImages/'.$newFileName);
+               $finalImg = "../images/registerShopImages/" . $newFileName;              
+            }  
+            // insert query
+            // $insertQry = "INSERT INTO `tblshopregistration`(`ShopImg1`, `ShopImg2`, `ShopImg3`, `ShopImg4`, `ShopImg5`) VALUES ($finalImg)";
+            // mysqli_query($conn,$insertQry);
+
+            //echo $finalImg;
+            
+            // $insertQry = "INSERT INTO tblshopregistration (ShopImg1) VALUES ('$finalImg')";
+
+            $insertQry = "INSERT INTO tblshopimage (ImgName,ImgUrl) VALUES ('$fileName','$finalImg')";
+
+            $data=mysqli_query($conn,$insertQry);
+
+           // header('Location:../index.html');
+        }
+        else
+        {
+            echo'<script> alert("Invalid extension of file") </script> ';
+        }
+    }
+}
 
 ?>
+
+
+
 
 
