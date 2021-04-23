f393396e929249c80a142a88aae414132e42eda1
Deepak Raj <Deeepak9018@gmail.com>
2021-04-24 01:26:59 +0530

    1:26

--- Coding/scripts.php
+++ Coding/scripts.php
@@ -1,3 +1,6 @@
+	<!-- Modernizer js -->
+	<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+
 	<!-- JS Files -->
 	<script src="js/vendor/jquery-3.2.1.min.js"></script>
 	<script src="js/popper.min.js"></script>
